2bb4e7c976d70e4851a1f0152deca31d00ea7547
Chetanpreet Malhi <chemalhi@gmail.com>
2021-12-22 17:01:02 -0500

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div className="link-section">
+          TEST
+        <a href="#demo">Demo</a>
+      </div>
+    </div>
+  );
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from './components/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="home" element={<Home />} />
+                <Route path="demo" element={<Home />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
