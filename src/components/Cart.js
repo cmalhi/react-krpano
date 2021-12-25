6684609047c568b38e97ff75a82ecab3dd40c973
Chetanpreet Malhi <chemalhi@gmail.com>
2021-12-25 00:08:22 -0500

    style updates

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,18 +2,40 @@
 import './Cart.scss';
 import React from 'react';
 import Button from '@mui/material/Button';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+
+const style = {
+    list: {
+        width: "100%",
+        maxWidth: 360,
+        bgcolor: "white"
+    },
+    listItem: {
+        height: 50,
+        size: 12
+    },
+    listItemText: {
+        size: 12
+    }
+};
 
 const Cart = (props) => {
   const cartItems = props.cartItems.map(cartItem =>
-      <div className="cart-item" key={cartItem.id}>
-          <div className="cart-item__image">x</div>
-          <div className="cart-item__text">
-              <div className="cart-item__name">{cartItem.name}</div>
-              <div className="cart-item__values">
-                  <div className="cart-item__price">{cartItem.price}</div>
-                  <div className="cart-item__quantity">x{cartItem.quantity}</div>
+      <div>
+          <ListItem sx={style.listItem} className="cart-item" key={cartItem.id}>
+              <div className="cart-item__image">x</div>
+              <div className="cart-item__text">
+                  <ListItemText sx={style.listItemText} primary={cartItem.name} className="cart-item__name" />
+                  <div className="cart-item__values">
+                      <div className="cart-item__price">{cartItem.price}</div>
+                      <div className="cart-item__quantity">x{cartItem.quantity}</div>
+                  </div>
               </div>
-          </div>
+          </ListItem>
+          <Divider />
       </div>
   );
   return (
@@ -21,9 +43,9 @@ const Cart = (props) => {
           <div className="cart__header">
               Your Cart
           </div>
-          <div className="cart__items">
+          <List sx={style.list} component="nav" aria-label="mailbox folders" className="cart__items">
               {cartItems}
-          </div>
+          </List>
           <Button
               variant="contained"
               className="cart__checkout"
